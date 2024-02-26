75850a95ad6780b2988c9fd3939390bcf249a598
pocaffeison <pocaffeison@gmail.com>
2024-02-26 16:20:13 +0900

    first commit

---
+++ src/test/java/com/example/pocaffesy/api/ApiApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.pocaffesy.api;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		System.out.println("Test Case");
+	}
+
+}
